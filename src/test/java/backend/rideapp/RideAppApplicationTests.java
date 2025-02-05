31c91306e0699b48775c6462eb130d750f542227
trivuniceva <trivunic99@gmail.com>
2025-02-05 20:17:58 +0100

    Initial commit

---
+++ src/test/java/backend/rideapp/RideAppApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.rideapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
