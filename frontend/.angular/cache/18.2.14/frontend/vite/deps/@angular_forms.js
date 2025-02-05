808a832d97ae31ddab953300b8a3367f86ff3bdd
trivuniceva <trivunic99@gmail.com>
2025-02-06 00:32:44 +0100

    mapa i homepage

--- frontend/.angular/cache/18.2.14/frontend/vite/deps/@angular_forms.js
+++ frontend/.angular/cache/18.2.14/frontend/vite/deps/@angular_forms.js
@@ -1,6 +1,6 @@
 import {
   getDOM
-} from "./chunk-LC63JU7F.js";
+} from "./chunk-ZKD5T45B.js";
 import {
   ChangeDetectorRef,
   Directive,
@@ -21,8 +21,6 @@ import {
   SkipSelf,
   Subject,
   Version,
-  __spreadProps,
-  __spreadValues,
   booleanAttribute,
   computed,
   forkJoin,
@@ -47,7 +45,11 @@ import {
   ɵɵdirectiveInject,
   ɵɵgetInheritedFactory,
   ɵɵlistener
-} from "./chunk-SXHOZHTL.js";
+} from "./chunk-KWYVCPYC.js";
+import {
+  __spreadProps,
+  __spreadValues
+} from "./chunk-3OV72XIM.js";
 
 // node_modules/@angular/forms/fesm2022/forms.mjs
 var BaseControlValueAccessor = class _BaseControlValueAccessor {
--- frontend/src/app/features/map/map.component.ts
+++ frontend/src/app/features/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -8,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrl: './map.component.css'
 })
 export class MapComponent {
+  map: any;
+  vehicles = [
+    { id: 1, lat: 45.2671, lng: 19.8335, status: 'slobodno' },  // Primer vozila u Novom Sadu
+    { id: 2, lat: 45.2695, lng: 19.8325, status: 'zauzeto' }
+  ];
+
+  ngOnInit(): void {
+    this.initializeMap();
+  }
+
+  initializeMap(): void {
+    this.map = L.map('map').setView([45.2671, 19.8335], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    // Definiši custom ikonu za marker
+    const customIcon = L.icon({
+      iconUrl: 'assets/leaflet-icons/marker-icon.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowUrl: 'assets/leaflet-icons/marker-shadow.png',
+      shadowSize: [41, 41]
+    });
+
+    // Dodaj vozila na mapu sa custom ikonom
+    this.vehicles.forEach(vehicle => {
+      const marker = L.marker([vehicle.lat, vehicle.lng], { icon: customIcon }).addTo(this.map);
+      marker.bindPopup(`<b>Vozilo ID: ${vehicle.id}</b><br>Status: ${vehicle.status}`);
+    });
+  }
+
 
 }
