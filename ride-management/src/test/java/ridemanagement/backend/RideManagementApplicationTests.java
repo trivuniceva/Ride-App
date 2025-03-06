95cf6ecf6ea7bd8f4d76dffba58d12ca9f1a456b
trivuniceva <trivunic99@gmail.com>
2025-03-06 17:53:18 +0100

    ride module

---
+++ ride-management/src/test/java/ridemanagement/backend/RideManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ridemanagement.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
