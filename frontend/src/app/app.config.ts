25c49d60f48f5cb6b31a1c8cdf56387913b304e1
trivuniceva <trivunic99@gmail.com>
2025-03-06 17:30:13 +0100

    navbar for driver

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -9,7 +9,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
       provideZoneChangeDetection({ eventCoalescing: true }),
       provideRouter(routes),
-      provideHttpClient(), provideAnimationsAsync(),
+      provideHttpClient(),
+    provideAnimationsAsync(),
 
   ]
 };
