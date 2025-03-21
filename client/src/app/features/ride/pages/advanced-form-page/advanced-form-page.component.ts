b48dc98061a6239d3c4aba343a077f4f68271695
trivuniceva <trivunic99@gmail.com>
2025-03-21 20:59:14 +0100

    additional options

--- client/src/app/features/ride/pages/advanced-form-page/advanced-form-page.component.ts
+++ client/src/app/features/ride/pages/advanced-form-page/advanced-form-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {NgIf} from '@angular/common';
 import {RouteFormComponent} from '../../components/route-form/route-form.component';
 import {VehicleTypeComponent} from '../../components/vehicle-type/vehicle-type.component';
+import {AdditionalOptionsComponent} from '../../components/additional-options/additional-options.component';
 
 @Component({
   selector: 'app-advanced-form-page',
@@ -10,13 +11,15 @@ import {VehicleTypeComponent} from '../../components/vehicle-type/vehicle-type.c
     NgIf,
     RouteFormComponent,
     VehicleTypeComponent,
+    AdditionalOptionsComponent,
   ],
   templateUrl: './advanced-form-page.component.html',
   styleUrl: './advanced-form-page.component.css'
 })
 export class AdvancedFormPageComponent {
-
   currentStep: number = 1;
+  additionalOptions: { carriesBabies: boolean, carriesPets: boolean } = { carriesBabies: false, carriesPets: false };
+
 
   goToStep(step: number): void {
     this.currentStep = step;
@@ -24,5 +27,9 @@ export class AdvancedFormPageComponent {
 
   processPayment() {
 
+  }
+
+  handleOptionsChange(options: { carriesBabies: boolean, carriesPets: boolean }) {
+    this.additionalOptions = options;
   }
 }
